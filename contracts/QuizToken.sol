c66094879ef4b8f9f929a35538d6778d72ddf49e
Surafel Nigusie <163689088+surafeldev@users.noreply.github.com>
2024-09-22 00:13:02 +0300

    Update QuizToken.sol

--- contracts/QuizToken.sol
+++ contracts/QuizToken.sol
@@ -1,48 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "forge-std/Test.sol";
+import "../contracts/QuizToken.sol";
 
+contract QuizTokenTest is Test {
+    QuizToken token;
 
-contract QuizToken is ERC20 {
-
-    uint256 public rewardAmount = 50*10**18;
-    address public owner;
-    
-    mapping(address => uint256) public userStreak;
-
-    event QuizCompleted(address indexed user, uint256 tokensAwarded);
-
-    constructor() ERC20("QuizToken", "QUIZ") {
-       owner = msg.sender;
-        
+    function setUp() public {
+        token = new QuizToken();
     }
 
-    // Function to reward users after they answer all questions correctly
-    function rewardUser(address user) public {
-        _mint(user, rewardAmount);
-        emit QuizCompleted(user, rewardAmount);
+    function testInitialSupply() public {
+        uint256 expectedSupply = 1000 * 10 ** token.decimals();
+        assertEq(token.totalSupply(), expectedSupply);
     }
 
-    // Function to allow users to spend tokens to retake the quiz
-    function spendTokensForRetake() public {
-        require(balanceOf(msg.sender) >= rewardAmount/2, "Insufficient tokens");
-        _burn(msg.sender, rewardAmount/2);
+    function testTokenName() public {
+        assertEq(token.name(), "QuizToken");
     }
 
-    // Function to set the reward amount
-    function setRewardAmount(uint256 _rewardAmount) public onlyOwner {
-        rewardAmount = _rewardAmount;
-    }
-
-    // Function to get the user's current token balance
-    function getUserBalance(address user) external view returns (uint256) {
-        return balanceOf(user);
-    }
-
-    //modifier for onlyowner
-    modifier onlyOwner(){
-        require(msg.sender == owner, "only owner can perform this.");
-        _;
+    function testTokenSymbol() public {
+        assertEq(token.symbol(), "QT");
     }
 }
